c09d9afb812a10d643d09a7a1a8e231397ee07b3
mark <israelatsenga@gmail.com>
2026-05-06 12:58:03 +0300

    feat: add event listener for send button on contact page to trigger sendMessage function

--- script.js
+++ script.js
@@ -60,4 +60,5 @@ const sendButton = document.querySelector("#send")
 sendButton.addEventListener("click", (event) => {
   // prevent form from refreshing page
   event.preventDefault();
+  sendMessage();
 });
